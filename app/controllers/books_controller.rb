8d5972ca74367fb304ffbd847b30a691cc9f80be
vagrant <vagrant@localhost.localdomain>
2020-06-05 14:00:26 +0000

    [Update]5-9

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,56 @@
+class BooksController < ApplicationController
+
+  def index
+  	@books = Book.all
+  	@book = Book.new
+    @user = current_user
+    @user.id = current_user.id
+  end
+
+  def show
+    @user = current_user
+    @user.id = current_user.id
+  	@book = Book.find(params[:id])
+  end
+
+  def new
+  	@books = Book.find(params[:id])
+  end
+
+  def create
+    @user = current_user
+  	@book = Book.new(book_params)
+    @book.user_id = current_user.id
+  	if @book.save
+  		redirect_to book_path(@book.id), notice: "You have creatad book successfully."
+  	else
+  		@books = Book.all
+  		render 'index'
+    end
+  end
+
+  def edit
+  	@book = Book.find(params[:id])
+  end
+
+  def update
+  	@book = Book.find(params[:id])
+  	if @book.update(book_params)
+  		redirect_to book_path(@book.id), notice: "You have updated book successfully."
+  	else
+  		render 'edit'
+    end
+  end
+
+  def destroy
+  	book = Book.find(params[:id])
+  	book.destroy
+  	redirect_to books_path, notice: "Book was succesfully destroyed."
+  end
+
+  private
+  def book_params
+  	params.require(:book).permit(:title, :body)
+  end
+
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+    @user = current_user
+    @user.id = current_user.id
+    @book = Book.new
+  end
+
+  def show
+    @book = Book.new
+  	@user = User.find(params[:id])
+    @books = @user.books
+  end
+
+  def edit
+  	@user = User.find(params[:id])
+  end
+
+  def update
+  	@user = User.find(params[:id])
+  	if @user.update(user_params)
+  	   redirect_to user_path(@user.id), notice: "You have updated user successfully."
+  	else
+  		render 'edit'
+  	end
+  end
+  
+  private
+  def user_params
+  	params.require(:user).permit(:name, :introduction, :profile_image, :id)
+  end
+end
